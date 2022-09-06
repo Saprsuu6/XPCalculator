556dd35e4830b97774950587afe20e9c8abc314d
AndrySaprigin <coffeei.2002@gmail.com>
2022-09-06 08:51:11 +0300

    continue proj

--- XPCalculator/Program.cs
+++ XPCalculator/Program.cs
@@ -1,9 +1,3 @@
 ﻿using XPCalculator.App;
 
 Console.WriteLine(RomanNumber.Parse("CM"));
-
-<<<<<<< HEAD
-// я удалил текст
-=======
-// этого текста тут не должно быть
->>>>>>> 4e82d1218ac389a656a2f37c9324e942e0dafe51
