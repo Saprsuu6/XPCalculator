c667df9465815312992a37955dc15cb5311bb917
AndrySaprigin <coffeei.2002@gmail.com>
2022-09-06 11:26:11 +0300

    Andry Saprigin 09.06.2022 (Have edit 5 line)

--- XPCalculator/Program.cs
+++ XPCalculator/Program.cs
@@ -2,5 +2,5 @@
 
 Console.WriteLine(RomanNumber.Parse("CM"));
 Console.WriteLine(RomanNumber.Parse("CD"));
-Console.WriteLine(RomanNumber.Parse("CC"));
+Console.WriteLine(RomanNumber.Parse("CCIC"));
 Console.WriteLine(RomanNumber.Parse("MCMXC"));
